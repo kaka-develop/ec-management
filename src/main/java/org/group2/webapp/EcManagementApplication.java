21ae724bc357673a0bdd3a6680c831bc757da868
Young Kaka <youngkaka@Youngs-MacBook-Pro.local>
2017-03-02 20:18:34 +0700

    config maven wrapper

--- src/main/java/org/group2/webapp/EcManagementApplication.java
+++ src/main/java/org/group2/webapp/EcManagementApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcManagementApplication.class, args);
+    }
 }
