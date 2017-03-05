071d945ac6ed935531b978265cb90520c74dd21f
dfChicken <dfChicken@nhn-pc>
2017-03-05 15:14:54 +0700

    update schema sql for assessment, course, claim

--- src/main/webapp/db/schema.sql
+++ src/main/webapp/db/schema.sql
@@ -4,6 +4,7 @@ DROP DATABASE IF EXISTS ecm_db;
 CREATE DATABASE ecm_db;
 USE ecm_db;
 
+-- INIT USER AND AUTHORITY TABLE --
 CREATE TABLE user (
   id            INTEGER      NOT NULL AUTO_INCREMENT PRIMARY KEY,
   username      VARCHAR(100) NOT NULL,
@@ -33,6 +34,61 @@ CREATE TABLE user_authority (
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8;
 
+-- INIT CLAIM, COURSE, ASSESSMENT TABLE --
+create table course(
+    `code` varchar(50) not null,
+    username varchar(100) not null,
+    primary key (id)
+)
+    engine = InnoDB
+    default character set = utf8;
+
+create table assessment(
+    crn varchar(50) not null,
+    course_code varchar(50) not null,
+    title varchar(100) not null,
+    primary key(crn),
+    foreign key (course_code) references course(code)
+)
+    engine = InnoDB
+    default character set = utf8;
+
+create table claim(
+    id int not null auto_increment,
+    user_id int,
+    date_started datetime,
+    date_ended datetime,
+    primary key(id),
+    foreign key (user_id) references `user`(id)
+)
+    engine = InnoDB
+    default character set = utf8;
+
+create table assessment_claim(
+    claim_id int not null,
+    assessment_crn varchar(50) not null,
+    solution1 int,
+    solution2 int,
+    primary key(claim_id, assessment_crn),
+    foreign key (claim_id) references claim(id),
+    foreign key (assessment_crn) references assessment(crn)
+)
+    engine = InnoDB
+    default character set = utf8;
+
+create table claim_circumstances(
+    claim_id int not null,
+    circumstances_id int not null,
+    primary key(claim_id, circumstances_id),
+    foreign key (claim_id) references claim(id),
+    foreign key (circumstances_id) references circumstances(id)
+)
+    engine = InnoDB
+    default character set = utf8;
+
+
+-- INSERT SAMPLE DATA FOR USER--
+
 INSERT INTO authority (name, created_date) VALUES
   ('ROLE_USER', NOW()), ('ROLE_STUDENT', NOW()), ('ROLE_MANAGER', NOW()), ('ROLE_COORDINATOR', NOW()),
   ('ROLE_ADMIN', NOW());
@@ -49,7 +105,7 @@ INSERT INTO user (username, password_hash, first_name, last_name, email, created
 INSERT INTO user_authority (user_id, authority_name) VALUES
   (1, 'ROLE_USER'), (2, 'ROLE_STUDENT'), (3, 'ROLE_COORDINATOR'), (4, 'ROLE_ADMIN');
 
-
+-- INSERT SAMPLE DATA FOR CLAIMS --
 
 
 
