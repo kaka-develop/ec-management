09d3361c2a4655aadaf454b8b94c8b56f15b7c85
dfChicken <dfChicken@nhn-pc>
2017-03-08 17:13:34 +0700

    update faculty entity

--- src/main/java/org/group2/webapp/entity/Faculty.java
+++ src/main/java/org/group2/webapp/entity/Faculty.java
@@ -10,6 +10,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 /**
@@ -24,4 +26,7 @@ public class Faculty implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
