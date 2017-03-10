9b222b3b910b6ce4eff70bb8b7f3f463ad22f2ad
Son <kunedo1104@gmail.com>
2017-03-10 11:49:51 +0700

    Add student controller. Submit claim. sending email for EC submit new claim view claims ui bootstrap

---
+++ src/main/java/org/group2/webapp/web/mvc/ctrl/StudentController.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package org.group2.webapp.web.mvc.ctrl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.group2.webapp.constraints.SessionKeys;
+import org.group2.webapp.entity.Assessment;
+import org.group2.webapp.entity.Circumstance;
+import org.group2.webapp.entity.Claim;
+import org.group2.webapp.entity.User;
+import org.group2.webapp.repository.AssessmentRepository;
+import org.group2.webapp.repository.CircumstanceRepository;
+import org.group2.webapp.repository.ClaimRepository;
+import org.group2.webapp.repository.UserRepository;
+import org.group2.webapp.web.util.MailUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Dam Cao Son
+ * @Contact kunedo1104@gmail.com
+ *
+ */
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+	private final UserRepository userRepo;
+	private final AssessmentRepository assessmentRepo;
+	private final ClaimRepository claimRepo;
+	private final CircumstanceRepository circumRepo;
+
+	public StudentController(UserRepository userRepo, AssessmentRepository assessmentRepo, ClaimRepository claimRepo,
+			CircumstanceRepository circumRepo) {
+		super();
+		this.userRepo = userRepo;
+		this.assessmentRepo = assessmentRepo;
+		this.claimRepo = claimRepo;
+		this.circumRepo = circumRepo;
+	}
+
+	@GetMapping("/claim")
+	public String viewClaim(HttpServletRequest req) {
+		User user = userRepo.findOneByUsername("student1").get();
+		req.getSession().setAttribute(SessionKeys.USER, user);
+		List<Claim> claims = getAllClaimOfStudent(user);
+		req.setAttribute("claims", claims);
+		return "claim/claims";
+	}
+
+	@GetMapping("/claim/add")
+	public String addClaim(HttpServletRequest req) {
+		User user = userRepo.findOneByUsername("student1").get();
+		req.getSession().setAttribute(SessionKeys.USER, user);
+		req.setAttribute("allAssessments", assessmentRepo.findAll());
+		req.setAttribute("allCircumstances", circumRepo.findAll());
+		return "claim/add";
+	}
+
+	@PostMapping("/claim/add")
+	public String addClaim(String[] assessments, long[] circumstances, @Valid Claim claim, BindingResult result,
+			HttpServletRequest req) {
+		if (assessments != null && circumstances != null && !result.hasErrors()) {
+			for (String ass : assessments) {
+				claim.getAssessment().add(assessmentRepo.getOne(ass));
+			}
+			for (long cid : circumstances) {
+				claim.getCircumstances().add(circumRepo.getOne(cid));
+			}
+			claim.setUser((User) req.getSession().getAttribute(SessionKeys.USER));
+			claimRepo.save(claim);
+			return "claim/success";
+		}else{
+			req.setAttribute("errors", result.getAllErrors());
+			addClaim(req);
+		}
+		return addClaim(req);
+	}
+
+	public List<Assessment> getAllAssetmentOfStudent(User student) {
+		// return assessmentRepo.findAll().stream().filter(a->student.get)
+		return null;
+	}
+
+	public List<Claim> getAllClaimOfStudent(User student) {
+		List<Claim> claims = claimRepo.findAll();
+		return claims.stream().filter(cl -> cl.getUser().getId() == student.getId()).collect(Collectors.toList());
+	}
+
+	public Optional<User> getECProcessClaim(Claim claim) {
+		// claim.getAssessment().iterator().next().getCourse().get
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
