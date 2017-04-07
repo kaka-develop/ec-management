0eca873545b141d9c27a4f73d4248ed91b839d55
dfChicken <dfChicken@nhn-pc>
2017-04-08 01:35:52 +0700

    merge UploadController to StudentController complete upload multiple evidences when add new claim

--- src/main/java/org/group2/webapp/web/mvc/ctrl/StudentController.java
+++ src/main/java/org/group2/webapp/web/mvc/ctrl/StudentController.java
@@ -1,16 +1,24 @@
 /**
- * 
+ *
  */
 package org.group2.webapp.web.mvc.ctrl;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
 import java.util.stream.Collectors;
+import javax.servlet.ServletContext;
 
 import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.FilenameUtils;
 
 import org.apache.log4j.Logger;
 import org.group2.webapp.constraints.ClaimStatusContraints;
@@ -32,6 +40,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author Dam Cao Son
@@ -41,87 +51,143 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/student/claim")
 public class StudentController {
-	private static final Logger logger = Logger.getLogger(StudentController.class);
-	@Autowired
-	private UserRepository userRepo;
-	@Autowired
-	private AssessmentRepository2 assessmentRepo;
-	@Autowired
-	private ClaimRepository claimRepo;
-	@Autowired
-	private CircumstanceRepository circumRepo;
-	@Autowired
-	private ItemRepository itemRepo;
 
-	@GetMapping("/view")
-	public String viewClaim(HttpServletRequest req) {
-		User currentUser = SessionUtils.getCurrentUserSession(userRepo).get();
-		List<String> str = Collections.list(req.getSession().getAttributeNames());
-		for (String s : str) {
-			System.out.println("session name: " + s);
-			System.out.println("value of: " + req.getSession().getAttribute(s));
-		}
-		List<Claim> claims = claimRepo.findAllByUserId(currentUser.getId());
-		Collections.sort(claims, new Comparator<Claim>() {
+    private static final Logger logger = Logger.getLogger(StudentController.class);
+    @Autowired
+    ServletContext servletContext;
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private AssessmentRepository2 assessmentRepo;
+    @Autowired
+    private ClaimRepository claimRepo;
+    @Autowired
+    private CircumstanceRepository circumRepo;
+    @Autowired
+    private ItemRepository itemRepo;
 
-			@Override
-			public int compare(Claim o1, Claim o2) {
-				
-				return o2.getCreated_time().compareTo(o1.getCreated_time());
-			}
-		});
-		
-		
-		req.setAttribute("claims", claims);
-		return "claim/claims";
-	}
+    @GetMapping("/view")
+    public String viewClaim(HttpServletRequest req) {
+        User currentUser = SessionUtils.getCurrentUserSession(userRepo).get();
+        List<String> str = Collections.list(req.getSession().getAttributeNames());
+        for (String s : str) {
+            System.out.println("session name: " + s);
+            System.out.println("value of: " + req.getSession().getAttribute(s));
+        }
+        List<Claim> claims = claimRepo.findAllByUserId(currentUser.getId());
+        Collections.sort(claims, new Comparator<Claim>() {
 
-	@GetMapping("/add")
-	public String addClaim(HttpServletRequest req) {
-		List<Assessment> suitableAssessment = assessmentRepo.findAll().stream()
-				.filter(ass -> ass.getFaculty().getId() == SessionUtils.getCurrentUserSession(userRepo).get()
-						.getFaculty().getId())
-				.collect(Collectors.toList());
+            @Override
+            public int compare(Claim o1, Claim o2) {
 
-		req.setAttribute("allAssessments", suitableAssessment);
-		req.setAttribute("allCircumstances", circumRepo.findAll());
-		logger.debug("suitableAssessment: " + suitableAssessment.size());
-		return "claim/add";
-	}
+                return o2.getCreated_time().compareTo(o1.getCreated_time());
+            }
+        });
 
-	@PostMapping("/add")
-	public String addClaim(String[] itemCrns, Long[] circumstances, String content,
-			HttpServletRequest req) {
-		if (itemCrns != null && circumstances != null && content != null) {
-			List<Circumstance> myCirumstance = circumRepo.findAll(Arrays.asList(circumstances));
-			User currentUser = SessionUtils.getCurrentUserSession(userRepo).get();
-			for (String ass : itemCrns) {
-				Item item = itemRepo.findOne(ass);
-				Claim myClaim = new Claim();
-				myClaim.setStatus(ClaimStatusContraints.PENDING);
-				myClaim.setUser(currentUser);
-				myClaim.setEvidence("file.pdf;anh.img");
-				myClaim.setContent(content);
-				myClaim.setItem(item);
-				myClaim.getCircumstances().addAll(myCirumstance);
-				claimRepo.save(myClaim);
-				System.out.println("Claim chuan bi save: " + myClaim);
-				MailUtils.sendClaimNewsForCoordinators(myClaim,
-						userRepo.findAllUserByAuthority(AuthoritiesConstants.COORDINATOR));
-			}
-			req.setAttribute("claimAdded", true);
-			return viewClaim(req);
-		} else {
-			return addClaim(req);
-		}
+        req.setAttribute("claims", claims);
+        return "claim/claims";
+    }
 
-	}
+    @GetMapping("/add")
+    public String addClaim(HttpServletRequest req) {
+        List<Assessment> suitableAssessment = assessmentRepo.findAll().stream()
+                .filter(ass -> ass.getFaculty().getId() == SessionUtils.getCurrentUserSession(userRepo).get()
+                        .getFaculty().getId())
+                .collect(Collectors.toList());
 
-	@GetMapping("/detail")
-	public String index(Long id, HttpServletRequest req) {
-		Claim claim = claimRepo.findOne(id);
-		System.out.println("claim: " + claim);
-		req.setAttribute("claim", claim);
-		return "claim/detail";
-	}
+        req.setAttribute("allAssessments", suitableAssessment);
+        req.setAttribute("allCircumstances", circumRepo.findAll());
+        logger.debug("suitableAssessment: " + suitableAssessment.size());
+        return "claim/add";
+    }
+
+    @PostMapping(path = "/add", headers = "Content-Type=multipart/*", consumes = "application/x-www-form-urlencoded")
+    public String addClaim(String[] itemCrns, Long[] circumstances, String content,
+            HttpServletRequest req, @RequestParam("evidenceFiles") MultipartFile[] files) {
+        if (itemCrns != null && circumstances != null && content != null) {
+            List<Circumstance> myCirumstance = circumRepo.findAll(Arrays.asList(circumstances));
+            User currentUser = SessionUtils.getCurrentUserSession(userRepo).get();
+            for (String ass : itemCrns) {
+                Item item = itemRepo.findOne(ass);
+                Claim myClaim = new Claim();
+                myClaim.setStatus(ClaimStatusContraints.PENDING);
+                myClaim.setUser(currentUser);
+                //upload evidences
+                String evidences = getEvidencesFromFileArray(files);
+                logger.info(evidences);
+                myClaim.setEvidence(evidences);
+
+                myClaim.setContent(content);
+                myClaim.setItem(item);
+                myClaim.getCircumstances().addAll(myCirumstance);
+                claimRepo.save(myClaim);
+                System.out.println("Claim chuan bi save: " + myClaim);
+                MailUtils.sendClaimNewsForCoordinators(myClaim,
+                        userRepo.findAllUserByAuthority(AuthoritiesConstants.COORDINATOR));
+            }
+            req.setAttribute("claimAdded", true);
+            return viewClaim(req);
+        } else {
+            return addClaim(req);
+        }
+
+    }
+
+    @GetMapping("/detail")
+    public String index(Long id, HttpServletRequest req) {
+        Claim claim = claimRepo.findOne(id);
+        System.out.println("claim: " + claim);
+        req.setAttribute("claim", claim);
+        return "claim/detail";
+    }
+
+    public String getEvidencesFromFileArray(MultipartFile[] files) {
+        int length = files.length;
+        String evidences = "";
+
+        for (int i = 0; i < length; i++) {
+            String name;
+            MultipartFile file = files[i];
+            try {
+                byte[] bytes = file.getBytes();
+
+                long time = System.currentTimeMillis();
+                java.sql.Timestamp timestmp = new java.sql.Timestamp(time);
+                name = "evidence" + i + "_" + timestmp.getTime() + "." + FilenameUtils.getExtension(file.getOriginalFilename());
+
+                // Creating the directory to store file
+                String actualPath = servletContext.getRealPath("");
+                String fileLocation = actualPath + File.separator + "evidents";
+
+                File dir = new File(fileLocation);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                }
+
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+
+                if (i == 0) {
+                    evidences = evidences + name;
+                } else if (i > 0) {
+                    evidences = evidences + ";" + name;
+                }
+
+                logger.info("Saved to: "
+                        + serverFile.getAbsolutePath());
+
+            } catch (FileNotFoundException ex) {
+                logger.info(ex.getMessage());
+            } catch (IOException ex) {
+                logger.info(ex.getMessage());
+            }
+        }
+        return evidences;
+    }
 }
