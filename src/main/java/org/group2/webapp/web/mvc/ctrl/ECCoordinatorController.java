6c452782ab7b1f5e0f30a3c5a26c4b23e85ca1eb
dfChicken <dfChicken@nhn-pc>
2017-04-15 15:44:27 +0700

    fix view uploaded evidences coordinator

--- src/main/java/org/group2/webapp/web/mvc/ctrl/ECCoordinatorController.java
+++ src/main/java/org/group2/webapp/web/mvc/ctrl/ECCoordinatorController.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.group2.webapp.web.mvc.ctrl;
 
@@ -33,47 +33,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/eccoordinator")
 public class ECCoordinatorController {
-	private static final Logger logger = LoggerFactory.getLogger(ECCoordinatorController.class);
 
-	@Autowired
-	private ClaimRepository claimRepo;
-	@Autowired
-	private UserRepository userRepo;
+    private static final Logger logger = LoggerFactory.getLogger(ECCoordinatorController.class);
 
-	@GetMapping("/claim")
-	public String index(HttpServletRequest req) {
-		User currentUser = SessionUtils.getCurrentUserSession(userRepo).get();
-		List<Claim> claims = claimRepo.findAllByFacultyId(currentUser.getFaculty().getId());
-		Collections.sort(claims, new Comparator<Claim>() {
+    @Autowired
+    private ClaimRepository claimRepo;
+    @Autowired
+    private UserRepository userRepo;
 
-			@Override
-			public int compare(Claim o1, Claim o2) {
-				return o2.getCreated_time().compareTo(o1.getCreated_time());
-			}
-		});
-		req.setAttribute("claims", claims);
-		return "claim/claims";
-	}
+    @GetMapping("/claim")
+    public String index(HttpServletRequest req) {
+        User currentUser = SessionUtils.getCurrentUserSession(userRepo).get();
+        List<Claim> claims = claimRepo.findAllByFacultyId(currentUser.getFaculty().getId());
+        Collections.sort(claims, new Comparator<Claim>() {
 
-	@GetMapping("/claim/process")
-	public String index(Long id, HttpServletRequest req) {
+            @Override
+            public int compare(Claim o1, Claim o2) {
+                return o2.getCreated_time().compareTo(o1.getCreated_time());
+            }
+        });
+        req.setAttribute("claims", claims);
+        return "claim/claims";
+    }
+
+    @GetMapping("/claim/process")
+    public String index(Long id, HttpServletRequest req) {
 		// User currentUser =
-		// SessionUtils.getCurrentUserSession(userRepo).get();
-		Claim claim = claimRepo.findOne(id);
-		System.out.println("claim: " + claim);
-		req.setAttribute("claim", claim);
-		return "claim/process";
-	}
+        // SessionUtils.getCurrentUserSession(userRepo).get();
+        Claim claim = claimRepo.findOne(id);
+        System.out.println("claim: " + claim);
+        req.setAttribute("claim", claim);
 
-	@PostMapping("/claim/process")
-	public String process(Long claimId, Integer status, String decision, HttpServletRequest req) {
-		Claim claim = claimRepo.findOne(claimId);
-		claim.setStatus(status);
-		claim.setDecision(decision);
-		claim.setProcessed_time(new Date());
-		claimRepo.save(claim);
-		MailUtils.sendClaimNewsForStudent(claim);
-		req.setAttribute("claimProcessed", true);
-		return index(req);
-	}
+        if (!claim.getEvidence().equals("")) {
+            String[] evidences = claim.getEvidence().split(";");
+            System.out.println("Evidences: " + evidences.length);
+            req.setAttribute("evidences", evidences);
+        }
+
+        return "claim/process";
+    }
+
+    @PostMapping("/claim/process")
+    public String process(Long claimId, Integer status, String decision, HttpServletRequest req) {
+        Claim claim = claimRepo.findOne(claimId);
+        claim.setStatus(status);
+        claim.setDecision(decision);
+        claim.setProcessed_time(new Date());
+        claimRepo.save(claim);
+        MailUtils.sendClaimNewsForStudent(claim);
+        req.setAttribute("claimProcessed", true);
+        return index(req);
+    }
 }
